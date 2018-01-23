1321deb12384eb0f5f28bc611661886130d7aed6
WLGQD <xiekaikai89@126.com>
2018-01-23 23:28:21 +0800

    test1

--- src/cn/how2j/HiWorld.java
+++ src/cn/how2j/HiWorld.java
@@ -2,6 +2,6 @@ package cn.how2j;
 
 public class HiWorld {
     public static void main(String args[]){
-        System.out.println("hello world!!");
+        System.out.println("hello world!!修改稿");
     }
 }
